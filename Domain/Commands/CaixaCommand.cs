eaa0e20a30aefc01869305059e2ee912e3c94d52
Carolina Lorenzon Neisse <cl.neisse@gmail.com>
2021-11-30 23:03:38 -0300

    usuario id

--- Domain/Commands/CaixaCommand.cs
+++ Domain/Commands/CaixaCommand.cs
@@ -7,7 +7,7 @@ namespace UStart.Domain.Commands
     {
         public Guid Id { get; set; }
         public DateTime DataCaixa { get; set; }
-        public Guid UsuarioId { get; set; }        
+        public Guid? UsuarioId { get; set; }        
         public Guid Usuario { get; set; }
         public Guid FormaPagamentoId { get; set; }        
         public String Observacao { get; set; }
